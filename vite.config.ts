5a3059ee4f81af55c53117676f22551e42d81c77
Taha Salman <tahasalman@Tahas-MacBook-Pro.local>
2025-08-08 18:06:30 +1000

    Build site for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,14 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // user/org pages
+  base: "/", // For user/org GitHub Pages (paradoxiscoding.github.io)
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  build: {
+    outDir: "docs", // Output build files to /docs for GitHub Pages
+    emptyOutDir: true, // Clears old build files before building
   },
 });
